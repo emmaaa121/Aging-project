11156577e3c1b891403ee745d3fe0a5e2d7fa774
emmaaa121 <104169587+emmaaa121@users.noreply.github.com>
2024-08-08 16:11:07 -0700

    Fig.5_Dynamo.py

--- Figures/Fig.5_Dynamo.py
+++ Figures/Fig.5_Dynamo.py
@@ -24,6 +24,7 @@ adata_combined = adata1.concatenate(adata2, batch_key='batch')
 adata_combined.write('/home/emma/data/10X_P7_2_3_marrow_bbknn.h5ad')
 
 adata = sc.read_h5ad('/home/emma/data/10X_P7_2_3_marrow_bbknn_annotated.h5ad')
+adata.X = adata.raw.X
 genes = ['Lars2', 'Mgp', 'Rpl13a',  'Ubb', 'Ets1', 'Cfl1', 'Tmsb10', 'S100a6', 'Rps29', 'Rps27', 'Gm6981', 'Map3k1', 'Wtap', 'Plp1', 'Pdcd4']
 preprocessor = Preprocessor(gene_append_list=genes)
 preprocessor.preprocess_adata(adata, recipe="monocle")
@@ -33,18 +34,19 @@ dyn.tl.dynamics(adata, model="stochastic")
 dyn.tl.reduceDimension(adata, n_pca_components=30)
 dyn.tl.louvain(adata, resolution=0.2)
 
-
 dyn.tl.cell_velocities(adata, method="pearson", other_kernels_dict={"transform": "sqrt"})
 dyn.tl.cell_velocities(adata, basis="pca")
+dyn.tl.cell_velocities(adata, basis="genovis")
 dyn.vf.VectorField(adata, basis='pca')
+dyn.vf.VectorField(adata, basis='genovis')
 print(adata)
 
 gene = 'Pdcd4'
-dyn.pd.perturbation(adata, gene, [-100], emb_basis="umap")
+dyn.pd.perturbation(adata, gene, [-100], emb_basis="genovis")
 print(adata)
 
 
-scv.pl.velocity_embedding_stream(adata, basis='umap_perturbation', color='cell_type', save='/home/emma/result/perturbation/Pdcd4_perturbation.png', dpi=300)
+scv.pl.velocity_embedding_stream(adata, basis='genovis_perturbation', color='cell_type', save='/home/emma/result/perturbation/Pdcd4_perturbation.png', dpi=300)
 
 # Gene specific velocity plots
 scv.pl.velocity(adata, ['Pdcd4'], perc = [5, 60], save='/home/emma/result/perturbation/Pdcd4_velocity_expression.png', dpi=300)
@@ -54,4 +56,9 @@ scv.pl.scatter(adata, basis= 'umap' , color= 'Pdcd4',  perc = [40, 95],  save='/
 scv.tl.velocity(adata)
 scv.tl.velocity_graph(adata)
 scv.tl.velocity_confidence(adata)
+keys = ['velocity_length', 'velocity_confidence']
 scv.pl.scatter(adata, c=keys, perc=[5, 95], save='/home/emma/result/perturbation/a_scvelo_velocity_length_confidence.png', dpi=300)
+
+# Velocity pseudotime
+scv.tl.velocity_pseudotime(adata)
+scv.pl.scatter(adata,basis= 'genovis', color='velocity_pseudotime', save='/home/emma/result/perturbation/a_scvelo_velocity_pseudotime.png', dpi=300)
